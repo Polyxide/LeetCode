7e4367c6c6692e71820b60fc5f159b3836e82d8c
Polyxide <nik.yakovlev.20@gmail.com>
2024-10-08 23:14:29 +0200

    add

--- algorithms.py
+++ algorithms.py
@@ -98,7 +98,7 @@ from collections import deque
 #
 #     while low <= high:
 #         mid = (low + high) // 2
-#         guess = list[int(mid)]
+#         guess = list[mid]
 #         if guess == item:
 #             return mid
 #         if guess > item:
@@ -285,35 +285,40 @@ from collections import deque
 
 
 
-#                                                                  Dynamic Programming
+#                                                                             Dynamic Programming
 
 
-def longest_common_substring(str1, str2):
-    # Get the lengths of both strings
-    m, n = len(str1), len(str2)
+# def longest_common_substring(str1, str2):
+#     # Get the lengths of both strings
+#     m, n = len(str1), len(str2)
+#
+#     # Create a 2D table to store lengths of longest common suffixes
+#     dp = [[0] * (n + 1) for _ in range(m + 1)]
+#     max_length = 0
+#     ending_index = 0  # To store the ending index of the longest common substring in str1
+#
+#     # Fill dp array
+#     for i in range(1, m + 1):
+#         for j in range(1, n + 1):
+#             if str1[i - 1] == str2[j - 1]:
+#                 dp[i][j] = dp[i - 1][j - 1] + 1
+#                 if dp[i][j] > max_length:
+#                     max_length = dp[i][j]
+#                     ending_index = i
+#             else:
+#                 dp[i][j] = 0
+#
+#     # The longest common substring is from ending_index - max_length to ending_index in str1
+#     return str1[ending_index - max_length: ending_index], max_length
+#
+#
+# # Example usage
+# str1 = "blue"
+# str2 = "clues"
+# lcs, length = longest_common_substring(str1, str2)
+# print(f"Longest Common Substring: '{lcs}' with length {length}")
 
-    # Create a 2D table to store lengths of longest common suffixes
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    max_length = 0
-    ending_index = 0  # To store the ending index of the longest common substring in str1
 
-    # Fill dp array
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-                if dp[i][j] > max_length:
-                    max_length = dp[i][j]
-                    ending_index = i
-            else:
-                dp[i][j] = 0
-
-    # The longest common substring is from ending_index - max_length to ending_index in str1
-    return str1[ending_index - max_length: ending_index], max_length
+#                                                                             K-Nearest
 
 
-# Example usage
-str1 = "blue"
-str2 = "clues"
-lcs, length = longest_common_substring(str1, str2)
-print(f"Longest Common Substring: '{lcs}' with length {length}")
